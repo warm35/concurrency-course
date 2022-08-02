e5acc8e3f1234fd08fc9c47cbdaa680a533e4e45
Nikolai Krivenko <nkrivenko35@gmail.com>
2022-08-02 09:40:51 +0300

    [2-4] Extract executor to class field, tune the timeout value

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -5,31 +5,46 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class PriceAggregator {
 
     private PriceRetriever priceRetriever = new PriceRetriever();
+    private ExecutorService executor;
 
     public void setPriceRetriever(PriceRetriever priceRetriever) {
         this.priceRetriever = priceRetriever;
     }
 
-    private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
+    private Collection<Long> shopIds;
+
+    public PriceAggregator() {
+        this(Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l));
+    }
+
+    public PriceAggregator(final Collection<Long> shopIds) {
+        this.shopIds = shopIds;
+        this.executor = Executors.newFixedThreadPool(shopIds.size());
+    }
 
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
+
+        this.executor.shutdown();
+        try {
+            this.executor.awaitTermination(1, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) { }
+
+        this.executor = Executors.newFixedThreadPool(shopIds.size());
     }
 
     public double getMinPrice(long itemId) {
-        var threadPoolExecutor = Executors.newFixedThreadPool(shopIds.size());
-
         var prices = shopIds.stream()
-            .map(shopId -> supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), threadPoolExecutor)
+            .map(shopId -> supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
                 .exceptionally(err -> Double.MAX_VALUE)
-                .completeOnTimeout(null, 2500, TimeUnit.MILLISECONDS))
+                .completeOnTimeout(null, 2970, TimeUnit.MILLISECONDS))
             .reduce(PriceAggregator::min);
 
         return prices.map(CompletableFuture::join).orElse(Double.NaN);
